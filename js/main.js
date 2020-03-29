396b470a2279a17192536ae64da948002c216556
Leandro Leberon <55678928+leandroleberon@users.noreply.github.com>
2020-03-28 22:31:52 -0400

    mod

---
+++ js/main.js
@@ -0,0 +1,8 @@
+jQuery(function ($) { "use strict";
+    $( document ).ready(function() {
+
+        // Smooth Scrolling
+        var scroll = new SmoothScroll('a[href*="#"]');
+        
+    });
+});
